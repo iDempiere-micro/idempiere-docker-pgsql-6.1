e20acaf6c86c75f760160dcdee7d3ada4e0d6399
davidpodhola <david@podhola.net>
2019-02-06 06:19:00 +0100

    also run the database

--- tools/idempiere_db.sh
+++ tools/idempiere_db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gzcat idempiere-docker-pgsql-6.2.0.latest.tar.gz | docker load
 docker volume create --name idempiere-pgsql-datastore
+docker run -d -v idempiere-pgsql-datastore:/data -p 5433:5432 -e PASS="postgres" idempiere/idempiere-docker-pgsql:6.2.0.latest
