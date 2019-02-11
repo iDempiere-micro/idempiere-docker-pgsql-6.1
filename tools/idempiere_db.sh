bdebd5b25bb1ef5934439382d3195f656402b158
davidpodhola <david@podhola.net>
2019-02-11 23:24:37 +0100

    do not download the docker file if not found

--- tools/idempiere_db.sh
+++ tools/idempiere_db.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ ! -f idempiere-docker-pgsql-6.2.0.latest.tar.gz ]; then
-    wget https://sourceforge.net/projects/idempiere-micro/files/docker-images/idempiere-docker-pgsql-6.2.0.latest.tar.gz/download
-fi
 zcat idempiere-docker-pgsql-6.2.0.latest.tar.gz | docker load
 docker volume create --name idempiere-pgsql-datastore
 docker run -d -v idempiere-pgsql-datastore:/data -p 5433:5432 -e PASS="postgres" idempiere/idempiere-docker-pgsql:6.2.0.latest
